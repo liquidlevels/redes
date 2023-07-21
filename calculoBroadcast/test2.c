685cf69dc24808d270a73a836aa6319118ab75ad
liquid <liquid>
2023-07-21 14:43:52 -0700

    Resolvi el problema con el conteo de puntos en ip, agregue el calculo de broadcast

--- calculoBroadcast/test2.c
+++ calculoBroadcast/test2.c
@@ -15,11 +15,11 @@ void function(){
 	char subnet_mask[16];
 	char* ip_octeto;
 	char* subnet_mask_octeto;
-	int dot1=0;
-	int dot2=0;
+	int dot=0;
 	int ip_octetos[5];
 	int subnet_mask_octetos[5];
 	int binario[33];
+	int broadcast[5];
 
 	printf("Formato de ip: 192.168.20.2\n");
 	printf("Ingrese la ip con el formato dado: ");
@@ -40,13 +40,13 @@ void function(){
 	//validar que la ip tenga exactamente tres puntos
 	for(int i = 0; i < 16; i++){
 		if( ip[i] == '.' )
-			dot1++;
+			dot++;
 	}
 
-	if(dot1 > 3 || dot1 < 3)
+	if(dot > 3 || dot < 3){
 		printf("Error, formato no aceptado\n");
-		printf("Hay un error en la ip dada, numero de puntos: %d\n\n", dot1);
-	
+		printf("Hay un error en la ip dada, numero de puntos: %d\n\n", dot);
+	}
 	//validar que en la ip no haya numeros negativos y que esten dentro del rango 0-255
 	ip_octeto = strtok(ip, ".");
 	for(int i = 0; i < 4; i++){
@@ -56,21 +56,23 @@ void function(){
 			//para que el array octetos no tenga informacion erronea cuando ocurre un error, se le asigna un 0 en la posicion del numero con formato no aceptado
 			ip_octetos[i] = 0;
 		}
-		else 
+		else{
 			ip_octetos[i] = atoi(ip_octeto); 
+		}
 		ip_octeto = strtok(NULL, ".");
 	}
 
+	dot=0;
 	//validar que la ip tenga exactamente tres puntos
 	for(int i = 0; i < 16; i++){
 		if( subnet_mask[i] == '.' )
-			dot2++;
+			dot++;
 	}
 
-	if(dot2 > 3 || dot2 < 3)
+	if(dot > 3 || dot < 3){
 		printf("Error, formato no aceptado\n");
-		printf("Hay un error en la subnet mask dada, numero de puntos: %d\n\n", dot2);
-
+		printf("Hay un error en la subnet mask dada, numero de puntos: %d\n\n", dot);
+	}
 	subnet_mask_octeto = strtok(subnet_mask, ".");
 	for(int i = 0; i < 4; i++){
 		printf("octeto %d -> %s\n", i+1, subnet_mask_octeto);
@@ -78,14 +80,34 @@ void function(){
 			printf("Error, formato no aceptado (0-255) en el octeto %d\n", i+1);
 			//para que el array octetos no tenga informacion erronea cuando ocurre un error, se le asigna un 0 en la posicion del numero con formato no aceptado
 			subnet_mask_octetos[i] = 0;
-		}
-		else 
+		} else {
 			subnet_mask_octetos[i] = atoi(subnet_mask_octeto); 
+		}
 		subnet_mask_octeto = strtok(NULL, ".");
 	}
 	
+	printf("ip: %d.%d.%d.%d\n\n", ip_octetos[0], ip_octetos[1], ip_octetos[2], ip_octetos[3]);
 	printf("subnet mask: %d.%d.%d.%d\n\n", subnet_mask_octetos[0], subnet_mask_octetos[1], subnet_mask_octetos[2], subnet_mask_octetos[3] );
 
+	//calculo del broadcast
+	int octeto_divertido = 0, nuevo_octeto = 0;
+	for(int i = 0; i < 4; i++){
+		if(subnet_mask_octetos[i] == 255){
+			broadcast[i] = ip_octetos[i];	
+		} else if(subnet_mask_octetos[i] == 0){
+			broadcast[i] = 255;
+		} else{
+			octeto_divertido = 256 - subnet_mask_octetos[i];
+			while(octeto_divertido < ip_octetos[i]){
+				nuevo_octeto = nuevo_octeto + octeto_divertido;
+			}
+			if(nuevo_octeto > ip_octetos[i]){
+				broadcast[i] = nuevo_octeto - 1;
+			}
+		}
+	}
+	printf("broadcast: %d.%d.%d.%d\n\n", broadcast[0], broadcast[1], broadcast[2], broadcast[3]);
+	
 	//conversion a binario
 	for (int i = 0; i < 4; i++) {
         	for (int j = 7; j >= 0; j--) {
@@ -102,8 +124,6 @@ void function(){
         	}
     	}
 	printf("\n\n");
-
-	//calculo del broadcast
 }
 int main(){
 	function();
