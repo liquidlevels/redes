c4931ca322d05488108d495f4062baf5e5a58d8a
liquid <liquid>
2023-08-03 02:27:45 -0700

    Ahora tiene validacion por si meten un numero menor o mayor al length del array

--- examen/examen1.c
+++ examen/examen1.c
@@ -12,9 +12,9 @@
 int digits[10];
 int dividedNumber[10] = {0,1,2,3,4,5,6,7,8,9};
 int vectorLength = sizeof(digits) / sizeof(digits[0]);
-
 void splitNumber(int number){
 	//thank to chatgpt and https://www.log2base2.com/c-examples/loop/split-a-number-into-digits-in-c.html
+	printf("length: %d\n", vectorLength);
 	int i = vectorLength;
 	while(number > 0){
 		int split = number % 10;
@@ -31,7 +31,13 @@ void splitNumber(int number){
 	printf("\n");*/
 }
 
-void sum(int position, int sumValue){
+int sum(int position, int sumValue){
+	
+	if(position > vectorLength-1 || position < vectorLength){
+		printf("error, number higher or lower than array length\n");
+		return 0;
+	}
+
 	printf("before sum ");
 	for(int i = 0; i < vectorLength; i++){	
 		printf("[%d] ", digits[i+1]);
@@ -45,11 +51,12 @@ void sum(int position, int sumValue){
 		printf("[%d] ", digits[i+1]);
 	}
 	printf("\n");
-
+	return 0;
 }
 
 int main(){
 	splitNumber(1234567896);
-	sum(0,3);
+	sum(10,32);
+	sum(-1, 2);
 	return 0;
 }
